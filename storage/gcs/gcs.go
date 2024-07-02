4637d9179eb67b00fb3ff5dbd704489ae752e3f6
Al Cutter <al@google.com>
2024-07-02 16:45:54 +0100

    More precise range

--- storage/gcs/gcs.go
+++ storage/gcs/gcs.go
@@ -23,6 +23,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -469,7 +470,8 @@ func (s *Storage) assignSequenceAndIntegrate(ctx context.Context) (bool, error) 
 			}
 			klog.Infof("SA: Sequencing & integrate: did %d @ %.1f qps took %0.1fs [r:%vs s:%vs i:%vs q:%vs]", numAdded, qps, d, f(now, readDone), f(readDone, sequenceDone), f(sequenceDone, integrateDone), f(integrateDone, sqlDone))
 		}()
-		rows := txn.Read(ctx, "Seq", spanner.Key{0}.AsPrefix(), []string{"seq", "v"})
+		// rows := txn.Read(ctx, "Seq", spanner.Key{0}.AsPrefix(), []string{"seq", "v"})
+		rows := txn.Read(ctx, "Seq", spanner.KeyRange{Start: spanner.Key{0, fromSeq}, End: spanner.Key{0, math.MaxInt64}}, []string{"seq", "v"})
 		defer rows.Stop()
 
 		seqsConsumed := []int64{}
